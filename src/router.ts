b2d5ba7c55013f2d54c1ad9899806c3afa04f2dc
Anton <rumpil666@gmail.com>
2023-11-10 08:17:44 +0400

    add analysis query

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import { notFoundComp } from './modules/notFound/notFound';
 import { homepageComp } from './modules/homepage/homepage';
 import { productDetailComp } from './modules/productDetail/productDetail';
 import { checkoutComp } from './modules/checkout/checkout';
+import { analysis } from './utils/eventAnalysis';
 
 const ROUTES = {
   '/': homepageComp,
@@ -27,6 +28,8 @@ export default class Router {
 
     // @ts-ignore
     const component = ROUTES[window.location.pathname] || notFoundComp;
+
+    analysis('route', { url: window.location.href })
 
     component.attach(this.$appRoot);
     component.render();
